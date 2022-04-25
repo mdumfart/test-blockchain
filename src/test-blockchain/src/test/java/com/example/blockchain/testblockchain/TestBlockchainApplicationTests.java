9b063440383757df5482c1d00a7c21a5b4962dc1
mdumfart <michi.dumfart@gmail.com>
2022-04-25 16:28:11 +0200

    init spring app

---
+++ src/test-blockchain/src/test/java/com/example/blockchain/testblockchain/TestBlockchainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blockchain.testblockchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBlockchainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
